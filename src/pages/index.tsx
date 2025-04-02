4467899d83d0b12ef56a6c3d3da8a30b47701bfc
anggaaprianda04 <anggaaprianda04@gmail.com>
2025-04-03 01:57:00 +0700

    feat: update next js 15

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
       <PageHead title="Home" />
       <Button color="primary">Click</Button>
     </main>
