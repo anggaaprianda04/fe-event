d7d84de904bb932ffa5ac960c9da7d588e97e7fe
anggaaprianda04 <anggaaprianda04@gmail.com>
2025-05-09 22:37:30 +0700

    feat: detail category

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -6,6 +6,7 @@ const categoryService = {
     getCategories: (params?: string) => instance.get(`${endpoint.CATEGORY}?${params}`),
     addCategory: (payload: ICategory) => instance.post(endpoint.CATEGORY, payload),
     deleteCategory: (id: string) => instance.delete(`${endpoint.CATEGORY}/${id}`),
+    getCategoryById: (id: string) => instance.get(`${endpoint.CATEGORY}/${id}`),
 };
 
 export default categoryService;
