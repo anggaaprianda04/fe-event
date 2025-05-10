3887271a572f1b3d78c3334045c6dee29e225272
anggaaprianda04 <anggaaprianda04@gmail.com>
2025-05-10 13:30:30 +0700

    feat: update category

--- src/components/views/Admin/DetailCategory/useDetailCategory.tsx
+++ src/components/views/Admin/DetailCategory/useDetailCategory.tsx
@@ -1,23 +1,62 @@
+import { ToasterContext } from "@/contexts/ToasterContext";
 import categoryService from "@/services/category.service";
-import { useQuery } from "@tanstack/react-query";
+import { ICategory } from "@/types/Category";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 
 const useDetailCategory = () => {
   const { query, isReady } = useRouter();
+  const { setToaster } = useContext(ToasterContext);
 
   const getCategoryById = async (id: string) => {
     const { data } = await categoryService.getCategoryById(id);
     return data.data;
   };
 
-  const { data: dataCategory } = useQuery({
+  const { data: dataCategory, refetch: refecthCategory } = useQuery({
     queryKey: ["Category"],
     queryFn: () => getCategoryById(`${query.id}`),
     enabled: isReady,
   });
 
+  const updateCategory = async (payload: ICategory) => {
+    const { data } = await categoryService.updatedCategoryById(
+      `${query.id}`,
+      payload,
+    );
+    return data.data;
+  };
+
+  const {
+    mutate: mutateUpdateCategory,
+    isPending: isPendingMutateUpdateCategory,
+    isSuccess: isSuccessMutateUpdateCategory,
+  } = useMutation({
+    mutationFn: (payload: ICategory) => updateCategory(payload),
+    onError: (error) => {
+      setToaster({
+        type: "error",
+        message: error.message,
+      });
+    },
+    onSuccess: () => {
+      refecthCategory();
+      setToaster({
+        type: "success",
+        message: "Success update category",
+      });
+    },
+  });
+
+  const handleUpdateCategory = (data: ICategory) => mutateUpdateCategory(data);
+
   return {
     dataCategory,
+
+    handleUpdateCategory,
+    isPendingMutateUpdateCategory,
+    isSuccessMutateUpdateCategory,
   };
 };
 
